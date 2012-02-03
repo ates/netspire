be78abc53fb06e094373b6e6d5349eb7208f0a6c
Artem Teslenko <ates@ipv6.dp.ua>
2012-02-03 16:58:20 +0200

    Add tests for util module

--- apps/netspire/src/lib/util.erl
+++ apps/netspire/src/lib/util.erl
@@ -28,3 +28,21 @@ hex(N) when N >= 10, N < 16 -> $A + (N - 10).
 list_to_hex_string([]) -> [];
 list_to_hex_string([H | T]) ->
     to_hex(H) ++ list_to_hex_string(T).
+
+%% Tests
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+to_hex_test() ->
+    ?assert(to_hex(10) =:= "0A"),
+    ?assert(to_hex(16) =:= "10"),
+    ?assert(to_hex(255) =:= "FF").
+
+do_bxor_test() ->
+    ?assert(do_bxor(<<1,2>>, <<3,4>>) =:= <<2,6>>).
+
+binary_to_hex_string_test() ->
+    ?assert(binary_to_hex_string(<<"erlang">>) =:= "65726C616E67").
+
+-endif.
