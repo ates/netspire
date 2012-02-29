ac732d852366595d5206c2297d1045bb245494ee
Artem Teslenko <ates@ipv6.dp.ua>
2012-02-29 17:24:19 +0200

    Fix typo

--- apps/netspire/src/modules/eap/eap_reg.erl
+++ apps/netspire/src/modules/eap/eap_reg.erl
@@ -11,7 +11,7 @@
 -export([init/1, handle_call/3, handle_cast/2,
          handle_info/2, code_change/3, terminate/2]).
 
--define(REGISTRY, eap_register).
+-define(REGISTRY, eap_registry).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
